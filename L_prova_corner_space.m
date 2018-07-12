68636b2a9842a277a978463d0b746790a0133834
francescacollu <33123803+francescacollu@users.noreply.github.com>
2018-07-12 11:27:12 +0200

    fornisce una DM con le proprietà 'giuste'

--- L_prova_corner_space.m
+++ L_prova_corner_space.m
@@ -14,34 +14,73 @@ BlockSx = Sx;
 BlockSy = Sy;
 
 %jump operator
-b = [0 0; 2 0];
+b = [0 0; 1 0];
 BlockC = b;
 
 H = - kron(Sx, kron(Sx, kron(BlockI, BlockI))) ...
     - kron(BlockI, kron(Sx, kron(Sx, BlockI))) ...
     - kron(BlockI, kron(BlockI, kron(Sx, Sx))) ...
-    - 1/2*kron(Sy, kron(Sy, kron(BlockI, BlockI))) ...
-    - 1/2*kron(BlockI, kron(Sy, kron(Sy, BlockI))) ...
-    - 1/2*kron(BlockI, kron(BlockI, kron(Sy, Sy)));
+    - 0.5*(kron(Sy, kron(Sy, kron(BlockI, BlockI))) ...
+    +kron(BlockI, kron(Sy, kron(Sy, BlockI))) ...
+    +kron(BlockI, kron(BlockI, kron(Sy, Sy))));
 
-C = kron(b, kron(BlockI, kron(BlockI, BlockI))) ...
-    + kron(BlockI, kron(b, kron(BlockI, BlockI))) ...
-    + kron(BlockI, kron(BlockI, kron(b, BlockI))) ...
-    + kron(BlockI, kron(BlockI, kron(BlockI, b)));
+C1 = kron(b, kron(BlockI, kron(BlockI, BlockI)));
+C2 = kron(BlockI, kron(b, kron(BlockI, BlockI)));
+C3 = kron(BlockI, kron(BlockI, kron(b, BlockI)));
+C4 = kron(BlockI, kron(BlockI, kron(BlockI, b)));
 
-BlockI = kron(BlockI, kron(BlockI, kron(BlockI, BlockI)));
+I = kron(BlockI, kron(BlockI, kron(BlockI, BlockI)));
 
 %H' è la trasposta di H
-first = 1i * H' - gamma * 1/2 * C.' * conj(C);
-second = -1i * H - gamma * 1/2 * C' * C;
-third = gamma * kron(conj(C), C);
+first = 1i * H.' - gamma * 1/2 * C1.' * conj(C1) - gamma * 1/2 * C2.' * conj(C2) ...
+    - gamma * 1/2 * C3.' * conj(C3) - gamma * 1/2 * C4.' * conj(C4);
+second = -1i * H - gamma * 1/2 * (C1') * C1 - gamma * 1/2 * (C2') * C2 ...
+    - gamma * 1/2 * (C3') * C3 - gamma * 1/2 * (C4') * C4;
+third = gamma * kron(conj(C1), C1) + gamma * kron(conj(C2), C2) + ...
+    + gamma * kron(conj(C3), C3) + gamma * kron(conj(C4), C4);
 
-L = kron(first, BlockI) + kron(BlockI, second) + third;
+L = kron(first, I) + kron(I, second) + gamma * third;
 
-[Evect Spectrum] = eig(L);
-Spectrum = diag(Spectrum);
-SortedSpectrum = sort(Spectrum, 'ComparisonMethod', 'abs');
+[Evect, Spectrum] = eig(L);
+[SortedSpectrum, Indices] = sort(diag(Spectrum), 'ComparisonMethod', 'abs');
+EvectSorted = Evect(:, Indices);
 
 disp(SortedSpectrum);
+
+rhoss = EvectSorted(:, 1);
+szDimL = size(L, 1);
+sz = sqrt(szDimL);
+rho = reshape(rhoss, [sz, sz]);
+dm = rho;
+[Edm, SpectrumDM] = eig(dm);
+display(diag(SpectrumDM));
+
+display("Somma(lambda_i) = " + sum(diag(SpectrumDM)));
+
+%dmT = dm';
+%equal = isequal(dmT, dm);
+
+%if(equal)
+%    disp("p è hermitiana.");
+%end
+
+if(abs(dm-dm') < exp(-14))
+   disp("p è hermitiana");  %ordine di E-15
+end
+
+
+disp(trace(dm));
+
+dens_matr = dm/trace(dm)
+disp(trace(dens_matr));
+
+Sz = kron(Sz, kron(BlockI, kron(BlockI, BlockI)));
+
+expValueSz = trace(dens_matr * Sz)
+
+
+
+
+
 
 
